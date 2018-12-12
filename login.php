07b11470481a36e159ea8b497eccba4284f2dc58
sschachenhofer <sschachenhofer@student.tgm.ac.at>
2018-12-12 09:32:45 +0100

    Changed login.php

--- login.php
+++ login.php
@@ -1,44 +1,44 @@
-
 <?php
   session_start();
   include 'connect.php';
 ?>
+<!DOCTYPE html>
 <html>
-<body>
-<form action="login.php" method="post">
-  <p>Benutzername:</p>
-  <input type="text" name="benutzername">
-  <p>Passwort:</p>
-  <input type="password" name="passwort">
+    <head>
+        <title>ADIN - Login</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <p>Benutzername:</p>
+            <input type="text" name="username">
+            <p>Passwort:</p>
+            <input type="password" name="password">
+
+            <?php
+                if(isset($_POST["submitLogin"])) {
+                    $username = $conn->real_escape_string($_POST["username"]);
+                    $password = $conn->real_escape_string($_POST["password"]);
+
+                    //Durchführen der SQL-Abfrage
+                    $sql = "SELECT Email, password FROM Users_tbl WHERE Email='$benutzer' AND password='$passwort'";
+                    $result_obj = $conn->query($sql);
+                    $result_map = $result_obj->fetch_assoc();
+
+                    //Überprüfen ob der Login richtig ist
+                    if ($result_obj->num_rows > 0) {
+                        $_SESSION["benutzer"] = $user["UserId"];
+                        header("Location: index.php");
+                    } else {
+                        ?>
+                        <p style="color:red;">Benutzername oder Passwort inkorrekt</p>
+                        <?php
+                    }
+                }
+            ?>
 
 
-  <?php
-  if(isset($_POST["submitLogin"])) {
-    $benutzer = mysqli_real_escape_string($conn, $_POST["benutzername"]);
-    $passwort = mysqli_real_escape_string($conn, $_POST["passwort"]);
-    
-      //Durchführen der SQL-Abfrage
-      $sql = "SELECT Email, password FROM Users_tbl WHERE Email='$benutzer' AND password='$passwort'";
-      $result = $conn->query($sql);
-      $user= mysqli_fetch_assoc($result);
-      $resultCheck = mysqli_num_rows($result);
-
-      //Überprüfen ob der Login richtig ist
-      if($resultCheck == 0) {
-      ?>
-      <p style="color:red;">Benutzername oder Passwort inkorrekt</p>
-      <?php
-      }  else {
-        $_SESSION["benutzer"] = $user["UserId"];
-        header("Location: index.php");
-      }
-
-  }
-?>
-
-  <p><button type="submit" name="submitLogin">Login</button></p>
-</form>
-</body>
+            <p><button type="submit" name="submitLogin">Login</button></p>
+        </form>
+    </body>
 </html>
-
-
