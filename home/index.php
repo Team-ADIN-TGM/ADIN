78d35dd2203122dc1c8cb4bb80a2a5015d263fe2
Dominik Cerny <dcerny@student.tgm.ac.at>
2019-01-09 08:19:02 +0100

    navbar included

--- home/index.php
+++ home/index.php
@@ -5,6 +5,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/style.css">
     <meta charset="utf-8">
     <title>ADIN - Home</title>
 </head>
@@ -13,33 +15,66 @@
    
     if(isset($_SESSION["user"])) {
 ?>
-    <p>Sie sind erfolgreich angemeldet</p>
-    <form action="index.php" method="post">
+<nav class="adin navbar">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+                <div class="item">
+                    <img src="../img/logo.png" class="img">
+                </div>
+        </li>
+    </ul>
 
-        <button type="submit" name="mailbox">Mailboxen Verwaltung</button>
-        <button type="submit" name="userconf">Benutzer Verwaltung</button>
-        <button type="submit" name="groupconf">Gruppenmailbox Verwaltung</button>
-        <button type="submit" name="logout">Logout</button>
-    </form>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a href="../users/users.php" class="a">
+                <div class="item">
+                    <img src="../img/benutzer.png" class="img">
+                    <span>Benutzer</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="#" class="a">
+                <div class="item">
+                    <img src="../img/mail.png" class="img">
+                    <span>Mailboxen</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="#" class="a">
+                <div class="item">
+                    <img src="../img/personen.png" class="img">
+                    <span>Verteiler</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="#" class="a">
+                <div class="item">
+                    <img src="../img/at.png" class="img">
+                    <span>Domains</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+
+</nav>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
 
-<?php
-if(isset($_POST["logout"])) {
-    session_destroy();
-    unset($_SESSION['user']);
-    header("Location: ../login/login.php");
-}
-if(isset($_POST["mailbox"])) {
-    header("Location: mail.php");
-}
-if(isset($_POST["userconf"])) {
-    header("Location: ../users/users.php");
-}
-if(isset($_POST["groupconf"])) {
-    header("Location: group.php");
-}
-?>
 
 <?php
     } else {
--- login/login.php
+++ login/login.php
@@ -5,6 +5,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <title>ADIN - Login</title>
         <meta charset="utf-8">
     </head>
@@ -14,7 +15,6 @@
             <input type="text" name="username">
             <p>Passwort:</p>
             <input type="password" name="password">
-
             <?php
                 if(isset($_POST["submitLogin"])) {
                     $username = $conn->real_escape_string($_POST["username"]);
@@ -41,5 +41,8 @@
 
             <p><button type="submit" name="submitLogin">Login</button></p>
         </form>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
