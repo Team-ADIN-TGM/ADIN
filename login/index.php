9a974691d2fd1087247419d0a32ac8cb16def152
sschachenhofer <sschachenhofer@student.tgm.ac.at>
2019-01-09 08:20:30 +0100

    Merge pls

--- login/index.php
+++ login/index.php
@@ -1,9 +1,11 @@
 <?php
-  include 'connect.php';
+  session_start();
+  include '../connect.php';
 ?>
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <title>ADIN - Login</title>
         <meta charset="utf-8">
     </head>
@@ -13,30 +15,25 @@
             <input type="text" name="username">
             <p>Passwort:</p>
             <input type="password" name="password">
-
             <?php
                 if(isset($_POST["submitLogin"])) {
                     $username = $conn->real_escape_string($_POST["username"]);
                     $password = $conn->real_escape_string($_POST["password"]);
 
                     //Durchführen der SQL-Abfrage
-                    $sql = "SELECT * FROM Users_tbl WHERE Email='$username';";
-                    # AND password='$passwort'
+                    $sql = "SELECT * FROM Users_tbl WHERE Email='$username' AND password='$password';";
                     $result_obj = $conn->query($sql);
                     $userdata = $result_obj->fetch_assoc();
 
                     //Überprüfen ob der Login richtig ist
                     if ($result_obj->num_rows > 0) {
-                        session_start();
-                        $_SESSION["user"] = $userdata["UserID"];
-                        header("Location: index.php");
+                        
+                        $_SESSION["user"] = $userdata["UserId"];
+                        header("Location: ../home/index.php");
                     } else {
                         ?>
                         <p style="color:red;">Benutzername oder Passwort inkorrekt</p>
                         <?php
-                            session_destroy();
-                            echo("SQL Query: ".$sql);
-                            var_dump($userdata);
                     }
                 }
             ?>
@@ -44,5 +41,8 @@
 
             <p><button type="submit" name="submitLogin">Login</button></p>
         </form>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
--- testtable.php
+++ testtable.php
@@ -1,51 +1,90 @@
 <?php
-  
-  include 'connect.php';
-?>
+    session_start();
+    include 'connect.php';
+    if(isset($_SESSION["user"])) {
 
-<?php
+      echo "<b>Users_tbl</b><br />";
+      $sql = "SELECT * FROM Users_tbl";
+      echo "<table border=5>";
+      echo "<th>UserId</th><th>DomainID</th><th>password</th><th>Email</th>";
+      foreach ($conn->query($sql) as $row) {
+         echo "<tr>";
+         echo "<td align='left'>" . $row['UserId'] . "</td>";
+         echo "<td align='left'>" . $row['DomainId'] . "</td>";
+         echo "<td align='left'>" . $row['password'] . "</td>";
+         echo "<td align='left'>" . $row['Email'] . "</td>";
 
-echo "<b>Users_tbl</b><br />";
-$sql = "SELECT * FROM Users_tbl";
-echo "<table border=5>";
-echo "<th>UserId</th><th>DomainID</th><th>password</th><th>Email</th>";
-foreach ($conn->query($sql) as $row) {
-   echo "<tr>";
-   echo "<td align='left'>".$row['UserId']."</td>";
-   echo "<td align='left'>".$row['DomainId']."</td>";
-   echo "<td align='left'>".$row['password']."</td>";
-   echo "<td align='left'>".$row['Email']."</td>";
-   
-   echo "</tr>";
-}
-echo "</table>";
+         echo "</tr>";
+      }
+      echo "</table>";
 
-echo "<b>Alias_tbl</b><br />";
-echo "<table border=5>";
-echo "<th>AliasId</th><th>DomainID</th><th>Source</th><th>Destination</th><th>Created</th><th>Modified</th><th>Active</th>";
-$sql2 = "SELECT * FROM Alias_tbl";
-foreach ($conn->query($sql2) as $row) {
-   echo "<tr>";
-   echo "<td align='left'>".$row['AliasId']."<br />";
-   echo "<td align='left'>".$row['DomainId']."<br />";
-   echo "<td align='left'>".$row['Source']."<br />";
-   echo "<td align='left'>".$row['Destination']."<br />";
-   echo "<td align='left'>".$row['Created']."<br />";
-   echo "<td align='left'>".$row['Modified']."<br />";
-   echo "<td align='left'>".$row['Active']."<br /><br />";
-   echo "</tr>";
-}
-echo "</table>";
+      echo "<b>Alias_tbl</b><br />";
+      echo "<table border=5>";
+      echo "<th>AliasId</th><th>DomainID</th><th>Source</th><th>Destination</th><th>Created</th><th>Modified</th><th>Active</th>";
+      $sql2 = "SELECT * FROM Alias_tbl";
+      foreach ($conn->query($sql2) as $row) {
+         echo "<tr>";
+         echo "<td align='left'>" . $row['AliasId'] . "<br />";
+         echo "<td align='left'>" . $row['DomainId'] . "<br />";
+         echo "<td align='left'>" . $row['Source'] . "<br />";
+         echo "<td align='left'>" . $row['Destination'] . "<br />";
+         echo "<td align='left'>" . $row['Created'] . "<br />";
+         echo "<td align='left'>" . $row['Modified'] . "<br />";
+         echo "<td align='left'>" . $row['Active'] . "<br /><br />";
+         echo "</tr>";
+      }
+      echo "</table>";
 
-echo "<b>Domains_tbl</b><br />";
-echo "<table border=5>";
-echo "<th>DomainID</th><th>DomainName</th>";
-$sql2 = "SELECT * FROM Domains_tbl";
-foreach ($conn->query($sql2) as $row) {
-   echo "<tr>";
-   echo "<td align='left'>".$row['DomainId']."<br />";
-   echo "<td align='left'>".$row['DomainName']."<br /><br />";
-   echo "</tr>";
-}
+      echo "<b>Domains_tbl</b><br />";
+      echo "<table border=5>";
+      echo "<th>DomainID</th><th>DomainName</th>";
+      $sql2 = "SELECT * FROM Domains_tbl";
+      foreach ($conn->query($sql2) as $row) {
+         echo "<tr>";
+         echo "<td align='left'>" . $row['DomainId'] . "<br />";
+         echo "<td align='left'>" . $row['DomainName'] . "<br /><br />";
+         echo "</tr>";
+      }
+      echo "</table>";
+      ?>
+
+      <form action='testtable.php' method='post'>
+        <input type="text" name="UserId">
+        <input type="text" name="DomainId">
+        <input type="text" name="password">
+        <input type="text" name="Email">
+        <button type="submit" name="submitInsert">Insert</button>
+        <br>
+        <input type="text" name="UserId2">
+        <button type="submit" name="submitDelete">Delete</button>
+
+      </form>
+
+      <?php
+      if(isset($_POST["submitInsert"])) {
+         $userid = $conn->real_escape_string($_POST["UserId"]);
+         $domainid = $conn->real_escape_string($_POST["DomainId"]);
+         $password = $conn->real_escape_string($_POST["password"]);
+         $email = $conn->real_escape_string($_POST["Email"]);
+
+         //Durchführen der SQL-Abfrage
+         $sql = "INSERT INTO Users_tbl (UserId, DomainId, password, Email) VALUES ('$userid', '$domainid', '$password', '$email')";
+         if(mysqli_query($conn, $sql)){
+            echo "Records inserted successfully.";
+         } else{
+            echo "ERROR: Could not able to execute";
+         }
+      }
+        if(isset($_POST["submitDelete"])) {
+            $userid2 = $conn->real_escape_string($_POST["UserId2"]);
+            $sql = "DELETE FROM Users_tbl WHERE UserId = $userid2" ;
+            if (mysqli_query($conn, $sql)) {
+                echo "Record deleted successfully";
+            } else {
+                echo "Error deleting record: " . mysqli_error($conn);
+            }
+        }
+
+   }
 ?>
 
