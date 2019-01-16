771473c7500a01d212c8bc7cf1abd719878a4e23
sschachenhofer <sschachenhofer@student.tgm.ac.at>
2019-01-16 08:46:38 +0100

    Adjusted index.php to new css styles

--- home/index.php
+++ home/index.php
@@ -5,77 +5,96 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style/style.css">
-    <meta charset="utf-8">
-    <title>ADIN - Home</title>
+	<title>ADIN - Home</title>
+	<meta charset="utf-8">
+	
+	<!-- Stylesheets -->
+	<link type="text/css" rel="stylesheet" href="../style/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	<!-- Scripts -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php
    
     if(isset($_SESSION["user"])) {
 ?>
-<nav class="navbar adin">
-    <ul class="navbar-nav">
-        <li class="nav-item">
-                <div class="adin-navbar-item">
-                    <img src="../img/logo.png" class="adin-navbar-img">
-                </div>
-        </li>
-    </ul>
-
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a href="../users/index.php" class="a">
-                <div class="item">
-                    <img src="../img/benutzer.png" class="img">
-                    <span>Benutzer</span>
-                </div>
-            </a>
-        </li>
-    </ul>
-
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a href="#" class="a">
-                <div class="item">
-                    <img src="../img/mail.png" class="img">
-                    <span>Mailboxen</span>
-                </div>
-            </a>
-        </li>
-    </ul>
-
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a href="#" class="a">
-                <div class="item">
-                    <img src="../img/personen.png" class="img">
-                    <span>Verteiler</span>
-                </div>
-            </a>
-        </li>
-    </ul>
-
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a href="#" class="a">
-                <div class="item">
-                    <img src="../img/at.png" class="img">
-                    <span>Domains</span>
-                </div>
-            </a>
-        </li>
-    </ul>
-
-</nav>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
-
-
+<!-- Navigationsleiste -->
+	<nav class="navbar adin">
+		<a class="navbar-brand" href="../home/">
+			<img src="../img/logo.png" id="adin-navbar-logo">
+		</a>
+		
+		<div>
+			<div class="navbar-item adin-navbar-item">
+				<a class="adin-navbar-link" href="../users/">
+					<img class="adin-navbar-item-img" src="../img/benutzer.png">
+					<p class="adin-navbar-item-text">Benutzer</p>
+				</a>
+			</div>
+			<div class="navbar-item adin-navbar-item">
+				<a class="adin-navbar-link" href="../users/">
+					<img class="adin-navbar-item-img" src="../img/mail.png">
+					<p class="adin-navbar-item-text">Mailboxen</p>
+				</a>
+			</div>
+			<div class="navbar-item adin-navbar-item">
+				<a class="adin-navbar-link" href="../users/">
+					<img class="adin-navbar-item-img" src="../img/personen.png">
+					<p class="adin-navbar-item-text">Verteiler</p>
+				</a>
+			</div>
+			<div class="navbar-item adin-navbar-item">
+				<a class="adin-navbar-link" href="../users/">
+					<img class="adin-navbar-item-img" src="../img/at.png">
+					<p class="adin-navbar-item-text">Domains</p>
+				</a>
+			</div>
+		</div>
+	</nav>
+	
+	<!-- Haupt-Container -->
+	<div class="container-fluid">
+		
+		<h1 class="mt-3">Domains</h1>
+		
+		<!-- Übersichtstabelle -->
+		<table class="overview-table">
+			<tr>
+				<th class="overview-table-content-cell">Domain-ID</th>
+				<th class="overview-table-content-cell">Domain-Name</th>
+				<th class="overview-table-content-cell">Domain-Admin</th>
+				<th class="overview-table-content-cell">Email-Adresse des Domain-Admins</th>
+				<th class="overview-table-button-cell"></th>
+				<th class="overview-table-button-cell"></th>
+			</tr>
+			<tr>
+				<td class="overview-table-content-cell">1</td>
+				<td class="overview-table-content-cell">test.dns.or.at</td>
+				<td class="overview-table-content-cell">mfrank</td>
+				<td class="overview-table-content-cell">mfrank@flashbrother.net</td>
+				<!-- TODO: Links müssen die ID der Domain enthalten, damit die Daten aus der Datenbank ausgelesen/gelöscht werden können! -->
+				<td class="overview-table-button-cell">
+					<a href="update.html" target="_blank">
+						<img src="../img/edit.png" class="overview-table-edit-button" alt="Bearbeiten">
+					</a>
+				</td>
+				<td class="overview-table-button-cell">
+					<a href="delete.html" target="_blank">
+						<img src="../img/delete.png" class="overview-table-delete-button" alt="Löschen">
+					</a>
+				</td>
+			</tr>
+		</table>
+		
+		<a href="new.html" class="btn mt-5 adin-button overview-table-add-button">
+			<img src="../img/add.png" class="mr-3">
+			Neue Domain hinzufügen
+		</a>
+	</div>
 <?php
     } else {
 ?>
@@ -84,7 +103,5 @@
 <?php
     }
 ?>
-
-
-
-
+</body>
+</html>
