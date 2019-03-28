733a394e77d1662a8267b932e86d7a8b7589c6be
sschachenhofer <sschachenhofer@student.tgm.ac.at>
2019-03-28 10:31:52 +0100

    Added JS to mailbox overview page (required for Bootstrap)

--- home/index.php
+++ home/index.php
@@ -33,6 +33,11 @@ $conn = get_database_connection();
 	<link type="text/css" rel="stylesheet" href="../style/style.css">
 	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 
+    <!-- JavaScript - ausnahmsweise verwendet, weil erforderlich für Bootstrap für das Domain-Auswahfeld -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </head>
 <body>
 	<?php if (isset($_SESSION["user"])) {?>
@@ -101,7 +106,6 @@ $conn = get_database_connection();
 		-->
 		<table class="overview-table">
 			<tr>
-				<th class="overview-table-content-cell">Benutzer-ID</th>
 				<th class="overview-table-content-cell">Benutzername</th>
 				<th class="overview-table-content-cell">Voller Name</th>
 				<th class="overview-table-content-cell">Email-Adresse</th>
@@ -114,7 +118,6 @@ $conn = get_database_connection();
 				<th class="overview-table-button-cell"></th>
 			</tr>
 			<tr>
-				<td class="overview-table-content-cell">35</td>
 				<td class="overview-table-content-cell">mfrank</td>
 				<td class="overview-table-content-cell">Markus Frank</td>
 				<td class="overview-table-content-cell">mfrank@flashbrother.net</td>
